1426ace6ad7cdaa30012c96bb80175e8b5dcb4bb
Ganesh Sadudiya <ganeshsadudiya@gmail.com>
2026-01-14 14:10:12 +0530

    test: add job_title presence validation test

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Employee, type: :model do
     employee = Employee.new(job_title: "Engineer", country: "India", salary: 50000)
     expect(employee).not_to be_valid
   end
+  it "is invalid without job_title" do
+    employee = Employee.new(full_name: "Ganesh", country: "India", salary: 50000)
+    expect(employee).not_to be_valid
+  end
 end
